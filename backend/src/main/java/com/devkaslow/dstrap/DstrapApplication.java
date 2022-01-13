c2f73e4c8414878d08f3b56caef670c304520db6
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-01-12 23:13:42 -0300

    @therealkaslow [Iniciado o Projeto React]

---
+++ backend/src/main/java/com/devkaslow/dstrap/DstrapApplication.java
@@ -0,0 +1,13 @@
+package com.devkaslow.dstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstrapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devkaslow/dstrap/DstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkaslow.dstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
