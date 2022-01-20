1105b42c29318634e4a3e9999254efeeabde62da
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-01-20 20:55:29 -0300

    @therealkaslow [adicionado o BASE_URL e testar requisição]

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import ArtistCard from "components/navbar/ArtistCard";
 import Pagination from "components/navbar/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+  // FORMA QUE N DEVO FAZER //
+  axios.get(`${BASE_URL}/artistas`)
+          .then(response  => {
+            console.log(response.data);
+          });
+
   return (
     <>
       <Pagination />
