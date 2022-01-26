953e578dd5ffd935419fb6e7d305672b858c660d
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2022-01-26 01:53:05 -0300

    @therealkaslow [adicionado artistas #1]

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -11,13 +11,24 @@ INSERT INTO tb_movie(score, count, title, image) VALUES (4, 0, 'Gunna', 'https:/
 INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Kodak Black', 'https://i.imgur.com/7SEQG1O.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Teto', 'https://i.imgur.com/KUBONGy.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (3, 0, 'Chefin', 'https://i.imgur.com/Yr3kriN.jpg');
-INSERT INTO tb_movie(score, count, title, image) VALUES (4, 0, 'Mateca', 'https://i.imgur.com/xelUJHu.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (4, 0, 'Mateca', 'https://i.imgur.com/lSa5xw4.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Young Thug', 'https://i.imgur.com/nE01y9U.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, '21 Savage', 'https://i.imgur.com/U9yaHtA.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Bruxo', 'https://i.imgur.com/yM6fDnp.jpg');
-
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Raffa Moreira', 'https://i.imgur.com/RqWQH91.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Sidoka', 'https://i.imgur.com/adfercb.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Kaslow', 'https://i.imgur.com/bhFv0mT.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'QBRVDA', 'https://i.imgur.com/HNgKvi6.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Aka Rasta', 'https://i.imgur.com/LJN7olp.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Xamã', 'https://i.imgur.com/nh4bhSV.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Poze do Rodo', 'https://i.imgur.com/XHPPs1c.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (5, 0, 'Matue', 'https://i.imgur.com/tA84wky.jpg');
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 1, 5.0);
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 2, 4.0);
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 1, 3.0);
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 2, 3.0);
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (2, 3, 4.0);
+
+
+
+
